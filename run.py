470a596c51c2a3c5f2ee0a951cc71622ca08e408
geludwig <74719181+geludwig@users.noreply.github.com>
2022-04-27 21:56:52 +0200

    version 1

--- run.py
+++ run.py
@@ -2,10 +2,19 @@
 try:
     import urllib.request
     import os.path
+    import sys
     import tkinter as tk
 except ModuleNotFoundError as err:
     print('[ERROR] ', err, '. Install required module with "pip" command first (e.g. python3 -m pip install <module>).')
     exit()
+
+if sys.platform == 'darwin':
+    try:
+        urllib.request.urlretrieve(urlscript, filename='script.py')
+    except:
+        print('[ERROR] Your system runs MacOS. Install CA certificates first (certificate_verify_failed).')
+        exit()
+
 
 ### WEBPAGE ###
 urlscript = 'https://raw.githubusercontent.com/geludwig/DreamGuardAndDatagrabber/main/script.py'
