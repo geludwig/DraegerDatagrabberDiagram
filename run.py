d362273e917710e576a937abad06163d9ba7cc03
geludwig <74719181+geludwig@users.noreply.github.com>
2022-04-27 21:33:22 +0200

    Update run.py

--- run.py
+++ run.py
@@ -7,28 +7,36 @@ except ModuleNotFoundError as err:
     print('[ERROR] ', err, '. Install required module with "pip" command first (e.g. python3 -m pip install <module>).')
     exit()
 
+### WEBPAGE ###
+urlscript = 'https://raw.githubusercontent.com/geludwig/DreamGuardAndDatagrabber/main/script.py'
+
 ### GET PAGE ###
 try:
-    urlversion = urllib.request.urlopen('https://raw.githubusercontent.com/geludwig/DreamGuardAndDatagrabber/main/script.py')
+    urlversion = urllib.request.urlopen(urlscript)
     urlversion = urlversion.read()
     versionweb = [int(s) for s in urlversion.split() if s.isdigit()]
     versionweb = versionweb[0]
-except:
-    print('[ERROR] web request error, exiting.')
+except urllib.error.HTTPError as e:
+    print('[ERROR] HTTP error: ', e)
     exit()
+except urllib.error.URLError as e:
+     print('[ERROR] URL error: ', e)
+     exit()
 
-# NO SCRIPT IN CURRENT DIR
+# NO SCRIPT IN DIR
 if (os.path.exists('script.py') is False):
     try:
         print('[INFO] Downloading python script.')
-        urlscript = 'https://raw.githubusercontent.com/geludwig/DreamGuardAndDatagrabber/main/script.py'
         urllib.request.urlretrieve(urlscript, filename='script.py')
-        #filename, headers = urllib.request.urlretrieve(urlscript, filename='test.py')
-    except:
-        print('[ERROR] web request error, exiting.')
+    except urllib.error.HTTPError as e:
+        print('[ERROR] HTTP error: ', e)
+        exit()
+    except urllib.error.URLError as e:
+        print('[ERROR] URL error: ', e)
         exit()
 
-# CHECK VERSION IF SCRIPT IS AVAILABLE
+# SCRIPT IN DIR
+# CHECK VERSION
 else:
     with open('script.py', "r") as file:
         versionscript = int(file.readline().strip())
@@ -38,6 +46,7 @@ else:
             os.system('python3 script.py')
 # NEW VERSION AVAILABLE, UPDATE BOX, DOWNLOAD NW SCRIPT
         elif versionweb > versionscript:
+            print('[INFO] New version available.')
             root= tk.Tk()
             root.geometry('300x100')
             root.resizable(False, False)
@@ -47,11 +56,13 @@ else:
                 root.destroy()
                 try:
                     print('[INFO] Downloading updated python script.')
-                    urlscript = 'https://raw.githubusercontent.com/geludwig/DreamGuardAndDatagrabber/main/script.py'
                     urllib.request.urlretrieve(urlscript, filename='script.py')
                     os.system('python3 script.py')
-                except:
-                    print('[ERROR] web request error, exiting.')
+                except urllib.error.HTTPError as e:
+                    print('[ERROR] HTTP error: ', e)
+                    exit()
+                except urllib.error.URLError as e:
+                    print('[ERROR] URL error: ', e)
                     exit()
 
             def updateNo():
@@ -63,7 +74,6 @@ else:
             buttonYes.pack(pady=10)
             buttonNo.pack()
             root.mainloop()
-
 # VERSION EXCEPTION
         else:
             print('Something went wrong.')
